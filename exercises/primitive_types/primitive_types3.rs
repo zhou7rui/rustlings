ec2022ba259c5d44f39a030169c0fc05aa00369b
zhourui <zhourui@pinkfongchina.com>
2023-08-02 18:38:16 +0800

    fix verify

--- exercises/primitive_types/primitive_types3.rs
+++ exercises/primitive_types/primitive_types3.rs
@@ -3,7 +3,7 @@
 // Execute `rustlings hint primitive_types3` or use the `hint` watch subcommand for a hint.
 
 fn main() {
-    let a = [];
+    let a = ["Are we there yet?"; 10];
 
     if a.len() >= 100 {
         println!("Wow, that's a big array!");
